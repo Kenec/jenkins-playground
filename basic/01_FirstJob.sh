09f2e57d9708f8418acd44c95654da5025f8bac6
Kenec <nnamani.kenechukwu@gmail.com>
2019-02-18 12:59:15 +0100

    build on commit

--- basic/01_FirstJob.sh
+++ basic/01_FirstJob.sh
@@ -2,12 +2,17 @@
 
 basic() {
   echo "This is the start of the Jenkin Basic Script"
-  echo $FirstNumber+$SecondNumber
+  echo (($FirstNumber + $SecondNumber))
   echo "Thank you for executibg me! :)"
+}
+
+build_on_commit() {
+  echo "This should build on commit"
 }
 
 main() {
   basic
+  build_on_commit
 }
 
 basic
