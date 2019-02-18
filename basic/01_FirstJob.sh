8e8aa5a751a3a706d61364e848f80ba1e9bcc814
Kenec <nnamani.kenechukwu@gmail.com>
2019-02-18 13:09:16 +0100

    fix build on commit

--- basic/01_FirstJob.sh
+++ basic/01_FirstJob.sh
@@ -2,7 +2,7 @@
 
 basic() {
   echo "This is the start of the Jenkin Basic Script"
-  echo (($FirstNumber + $SecondNumber))
+  echo $FirstNumber + $SecondNumber
   echo "Thank you for executibg me! :)"
 }
 
