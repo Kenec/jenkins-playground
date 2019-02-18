c4971be43a488078834e9acaaf5640423067b835
Kenec <nnamani.kenechukwu@gmail.com>
2019-02-18 12:20:06 +0100

    setup git job on jenkins

---
+++ basic/01_FirstJob.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+basic() {
+  echo "This is the start of the Jenkin Basic Script"
+  echo $FirstNumber+$SecondNumber
+  echo "Thank you for executibg me! :)"
+}
+
+main() {
+  basic
+}
+
+basic
